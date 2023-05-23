98cc107ac9ed5d62997023618cdf2e6e24de9f84
Ayooluwa olosunde <ayooluwa.olosunde@gmail.com>
2023-05-23 21:12:15 +0100

    feature:fix dex issue

--- app/src/main/java/com/lovisgod/kozenlib/core/data/models/EmvResultKozen.kt
+++ app/src/main/java/com/lovisgod/kozenlib/core/data/models/EmvResultKozen.kt
@@ -1,10 +1,10 @@
-package com.interswitchng.smartpos.shared.models.transaction.cardpaycode
+package com.lovisgod.kozenlib.core.data.models
 
 /**
  * This class represents the status results from the
  * CardReader's communication with the Card Chip
  */
-enum class EmvResult(val code: Int) {
+enum class EmvResultKozen(val code: Int) {
     OFFLINE_DENIED(0), // AC_AAC = 0
     OFFLINE_APPROVED(1), // AC_TC = 1
     ONLINE_REQUIRED(2), // AC_ARQC = 2
